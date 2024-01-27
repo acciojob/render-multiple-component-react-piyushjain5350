0194e17fa4a72a46683ab431a1de2e28bccae883
Piyush Jain <piyushjain5350@gmail.com>
2024-01-27 23:52:35 +0530

    1 soln

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,12 @@ import '../styles/App.css';
 
 const App = () => {
   return (
-    <div id="main"></div>
+    <div id="main">
+      <h1 data-ns-test="project-name">Digital Vehicle Fuel Level Indicator</h1>
+
+<h2 data-ns-test="project-description">In our project the main blocks are micro controller unit, fuel level sensor and LCD display unit. The fuel level detection circuit is used to detect the level of the fuel in the tank; here sensors are placed at certain places to find out the fuel level and the signals are sent to the micro controller unit for further operations.</h2>
+
+    </div>
   )
 }
 
